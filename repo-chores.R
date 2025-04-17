05f53aa358086ab2bc6a881d44de5fc4b80c0ec7
Frie Preu <fr1e@pm.me>
2025-04-17 14:45:34 +0200

    update license info and add to make

--- repo-chores.R
+++ repo-chores.R
@@ -4,7 +4,10 @@ library(stringr)
 library(glue)
 
 upload <- readr::read_csv("data/upload.csv")
-done <- upload %>% filter(!str_detect(md, "An dieser Seite arbeiten wir noch"))
+kanban <- readr::read_csv("data/kanban.csv") %>% 
+  filter(str_detect(status, "Done"))
+
+done <- upload %>% filter(id %in% kanban$id)
 
 license_info <- c("# LIZENZ INHALTE\n", 
     "[![License: CC BY 4.0](https://img.shields.io/badge/License-CC%20BY%204.0-lightgrey.svg)](https://creativecommons.org/licenses/by/4.0/deed.de)\n",
